6f19f51e1615aff437965fb88fce666abd5cbaa8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-01-12 20:37:33 +0300

    designed the URL form

--- src/app/shortener/components/Shortener.vue
+++ src/app/shortener/components/Shortener.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="container container--centered">
-    Home
+    <form class="urlform" action="">
+      <input type="url" placeholder="Let's get you a short URL" class="urlform__input">
+    </form>
+
+    <div class="result">
+      <div class="result__details">
+        <div class="result__original">
+          <span>google.com</span>
+        </div>
+        <a href="#" target="_blank" class="result__url">http://localhost:5050/abc</a>
+        <a href="#" class="result__stats">Get stats</a>
+      </div>
+      <button class="button button--yellow">Copy to clipboard</button>
+    </div>
+
+    <!-- <div class="notice">
+      Hit return when you're done
+    </div>
+
+    <div class="notice">
+      One moment
+    </div> -->
   </div>
 </template>
 
@@ -9,3 +30,82 @@
 
   }
 </script>
+
+<style lang="scss">
+  .urlform {
+    width: 100%;
+    margin-bottom: 40px;
+
+    &__input {
+      font: inherit;
+      font-size: 1.2em;
+      padding: 20px;
+      width: 100%;
+      border: 0;
+      outline: none;
+      border-radius: 3px;
+      box-shadow: 0 0 50px rgba(#fff, .1)
+    }
+  }
+
+  .notice {
+    color: #fff;
+    font-size: 1.1em;
+    font-weight: 500;
+  }
+
+  .result {
+    width: 100%;
+    text-align: center;
+
+    &__details {
+      color: #fff;
+      font-size: 1.4em;
+    }
+
+    &__url {
+      display: block;
+      margin-bottom: 20px;
+      color: #fff;
+    }
+
+    &__original {
+      opacity: 0.5;
+      margin-bottom: 5px;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+
+      span {
+        display: inline-block;
+        max-width: 300px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        margin-right: 8px;
+      }
+    }
+
+    &__stats {
+      display: block;
+      color: #fff;
+      font-size: .9em;
+      margin-bottom: 20px;
+    }
+
+    @media (min-width: 736px) {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: center;
+
+      &__stats {
+        margin-bottom: 0;
+      }
+
+      &__details {
+        margin-right: 40px;
+      }
+    }
+  }
+</style>
--- src/app/stats/components/Stats.vue
+++ src/app/stats/components/Stats.vue
@@ -5,5 +5,29 @@
 </template>
 
 <script>
-
+  export default {
+    
+  }
 </script>
+
+<style lang="scss">
+  .stats {
+    color: #fff;
+    font-size: 1.2em;
+    font-weight: 500;
+    width: 100%;
+
+    &__header {
+      font-size: 1.4em;
+      margin: 0;
+      margin-bottom: 40px;
+      overflow-wrap: break-word;
+    }
+
+    &__loading {
+      color: #fff;
+      font-size: 1.2em;
+      font-weight: 500;
+    }
+  }
+</style>
